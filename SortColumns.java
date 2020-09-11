40d47fbd8d8f31fe37e66c65e4216d9c749c1f43
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-09-11 11:54:16 -0500

    Update SortColumns.java

--- SortColumns.java
+++ SortColumns.java
@@ -2,18 +2,42 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Declare an instance of List.
-// Since a List is an interface, we use ArrayList also.
-List<Integer> myList = new ArrayList<Integer>();
+public class SortColumns{
+	
+	public int [] minDeletionSize (int [] A){
 
-// Use a List's important functions: add, get.
-// The "add" function adds the content to the end.
-myList.add(12);
-myList.add(15);
+	}
 
+	public void add(int num){
 
-// The "get" function retrieves the data at the index provided
-// ... so this loop prints the data in order.
-for ( int i = 0; i < myList.size(); i++) {
-  System. out .println(myList.get(i));
+	}
+
+	public int[] get(int index){
+		// The "get" function retrieves the data at the index provided
+		// ... so this loop prints the data in order.
+		for ( int i = 0; i < myList.size(); i++) {
+		  System. out .println(myList.get(i));
+}
+	}
+	
+	public static void main(String[] args){
+		SortColumns sortColum = new SortColumns();
+
+		Scanner input=new Scanner(System.in);
+
+		System.out.println(" ");
+
+		System.out.println(" ");
+
+		// Declare an instance of List.
+		// Since a List is an interface, we use ArrayList also.
+		int [] myList = new int[n];
+
+		// Use a List's important functions: add, get.
+
+		// The "add" function adds the content to the end.
+		myList.add(12);
+		myList.add(15);
+
+	}
 }
